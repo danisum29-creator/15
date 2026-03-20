1f74049175ba53dbb5ec90f6d2d79e817d646be3
Daniel S. <edvindaniel427@gmail.com>
2026-03-20 08:32:34 -0600

    Agregar archivos de proyecto.

---
+++ 15/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _15
+{ 
+
+
+    class Dato
+    {
+        public int ValidarDato()
+        {
+            do
+            {
+                Console.WriteLine("1. agregar producto\n2. modificar producto\n3 eliminar producto\n4. buscar producto\n5. mostrar todos los productos\n6. salir "
+
+            }
+
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
